3d96a6a803b99bc8bd03aee0c1ce6f36c4056769
Rodrigo Henriques <1711319@sal.ipg.pt>
2025-03-18 12:07:07 +0000

    Add files via upload

---
+++ src/main/Act01.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author 6487
+ */
+public class Act01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+             
+        Para carregar o Menu.java é necessário intânciar o objeto
+        e torná-lo visivel Menu visivel.
+        
+        */
+        
+        Menu menuForm = new Menu(); // -- Instância de Menu.java  
+        
+        menuForm.setVisible(true); // --  Tornar 
+    }
+    
+}
